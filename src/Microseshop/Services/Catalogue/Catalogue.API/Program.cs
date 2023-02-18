6273449957d1f42842c3b78fdbaacd907e95f3ef
sigier <jotulw@proton.me>
2023-02-18 23:05:51 +0200

    Catalogue.API added

---
+++ src/Microseshop/Services/Catalogue/Catalogue.API/Program.cs
@@ -0,0 +1,30 @@
+using Catalogue.API.Data;
+using Catalogue.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICatalogueContext, CatalogueContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalogue.API v1")); ;
+}
+
+
+
+app.MapControllers();
+
+app.Run();
+
---
+++ src/Microseshop/Services/Catalogue/Catalogue.API/Repositories/ProductRepository.cs
@@ -0,0 +1,78 @@
+﻿using Catalogue.API.Data;
+using Catalogue.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalogue.API.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+        private readonly ICatalogueContext _context;
+
+        public ProductRepository(ICatalogueContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context
+                .Products
+                .Find(p => true)
+                .ToListAsync() as IEnumerable<Product>;
+        }
+        public async Task<Product> GetProduct(string id)
+        {
+           return await _context
+               .Products
+               .Find(p => p.Id == id)
+               .FirstOrDefaultAsync().ConfigureAwait(false) as Product;
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+            return await _context
+                            .Products
+                            .Find(filter)
+                            .ToListAsync() as IEnumerable<Product>; 
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+
+            return await _context
+                            .Products
+                            .Find(filter)
+                            .ToListAsync() as IEnumerable<Product>;
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await _context
+                                        .Products
+                                        .ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged
+                    && updateResult.ModifiedCount > 0;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await _context
+                                                .Products
+                                                .DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged
+                && deleteResult.DeletedCount > 0;
+        }
+    }
+}
