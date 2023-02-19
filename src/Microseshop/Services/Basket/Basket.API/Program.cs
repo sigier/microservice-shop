7753aadb54b5032c31c2dd2d52563030b9071e7e
sigier <jotulw@proton.me>
2023-02-19 14:52:15 +0200

    Basket.API added

---
+++ src/Microseshop/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,35 @@
+using Basket.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+ConfigurationManager configuration = builder.Configuration;
+
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = configuration.GetSection("CacheSettings:ConnectionString").Value;
+
+});
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapControllers();
+
+app.Run();
+
