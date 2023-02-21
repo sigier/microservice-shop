0f0dd06826df1363a1fcb9decd735f9c64978c4f
sigier <jotulw@proton.me>
2023-02-21 18:49:18 +0200

    Auto-migration added

--- src/Microseshop/Services/Ordering/Ordering.API/Ordering.API/Program.cs
+++ src/Microseshop/Services/Ordering/Ordering.API/Ordering.API/Program.cs
@@ -1,7 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
+using Ordering.API.Extensions;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+IHost host = Host.CreateDefaultBuilder(args).Build();
+host.MigrateDatabase<OrderContext>((ctx, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed
+        .SeedAsync(ctx, logger)
+        .Wait();
+
+});
 
 builder.Services.AddControllers();
+builder.Services.AddAppServices();
+builder.Services.AddInfrastructureServices(configuration);
+
 
 
 // Add services to the container.
