46f0537a5112684301b20e62eac4f6dc2b9b4d26
sigier <jotulw@proton.me>
2023-02-21 15:14:00 +0200

    OrderController added

---
+++ src/Microseshop/Services/Ordering/Ordering.API/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Ordering.Application.Features.Orders.Queries;
+using System.Net;
+using Ordering.Application.Features.Orders.Commands;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+
+        }
+
+        [HttpGet("{userName}", Name = "GetOrder")]
+        [ProducesResponseType(typeof(IEnumerable<OrderDto>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrdersByUserName(string userName)
+        {
+            GetOrdersListQuery query = new GetOrdersListQuery(userName);
+            List<OrderDto> orders = await _mediator.Send(query);
+            return Ok(orders);
+        }
+
+
+        [HttpPut(Name = "UpdateOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateOrder([FromBody] UpdateOrderCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            var command = new DeleteOrderCommand() { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+    }
+}
