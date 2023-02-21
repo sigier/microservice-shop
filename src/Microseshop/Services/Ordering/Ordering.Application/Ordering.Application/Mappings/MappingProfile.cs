99ecd8dfb9da81b620810b392f21c397f6de1f8d
sigier <jotulw@proton.me>
2023-02-21 12:38:58 +0200

    Query mediator part added

---
+++ src/Microseshop/Services/Ordering/Ordering.Application/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿
+using AutoMapper;
+using Ordering.Application.Features.Orders.Queries;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrderDto>().ReverseMap();
+        }
+        
+    }
+}
